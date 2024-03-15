90344035424ee7266f35ea84b49e302fe0c1288f
Konstantin Láska <konstantin.laska@icloud.com>
2024-03-15 12:03:05 +0100

    new

--- Displays/E-Paper/2-70/GDEW027W3_GxEPD2/GDEW027W3_GxEPD2.ino
+++ Displays/E-Paper/2-70/GDEW027W3_GxEPD2/GDEW027W3_GxEPD2.ino
@@ -23,7 +23,6 @@
 
 #define SLEEP_SEC 15         // Measurement interval (seconds)
 
-//GxEPD2_BW<GxEPD2_270_GDEY027T91, GxEPD2_270_GDEY027T91::HEIGHT> display(GxEPD2_270_GDEY027T91(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEY027T91 176x264, SSD1680
 GxEPD2_BW<GxEPD2_270, GxEPD2_270::HEIGHT> display(GxEPD2_270(/*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEW027W3 176x264, EK79652 (IL91874)
 
 void setup()
