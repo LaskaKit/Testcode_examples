38c5157ac895408adcfce4b10fd26af193f1c81a
Konstantin Láska <konstantin.laska@icloud.com>
2025-03-27 15:27:16 +0100

    cleaned a little bit

--- Displays/E-Paper/7-50/GDEY075T7_GxEPD2_4G/GDEY075T7_GxEPD2_4G.ino
+++ Displays/E-Paper/7-50/GDEY075T7_GxEPD2_4G/GDEY075T7_GxEPD2_4G.ino
@@ -19,12 +19,12 @@
 
 // base class GxEPD2_GFX can be used to pass references or pointers to the display instance as parameter, uses ~1.2k more code
 // enable or disable GxEPD2_GFX base class
-#define ENABLE_GxEPD2_GFX 1
+//#define ENABLE_GxEPD2_GFX 1
 
 #include <GxEPD2_4G_4G.h>
 #include <GxEPD2_4G_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
-
+#include "bitmaps/Bitmaps4g800x480.h" // 7.5"  b/w
 
 //#define ESPink_V2     //for version v2.6 and older
 #define ESPink_V3     //for version v3.0 and above
@@ -49,90 +49,33 @@
 
 GxEPD2_4G_4G<GxEPD2_750_GDEY075T7, GxEPD2_750_GDEY075T7::HEIGHT / 2> display(GxEPD2_750_GDEY075T7(/*CS=5*/ SS, /*DC=*/ DC, /*RST=*/ RST, /*BUSY=*/ BUSY)); // GDEY075T7  800x480, UC8179 (GD7965), (FPC-C001 20.08.20)
 
-
-#if !defined(__AVR) && !defined(_BOARD_GENERIC_STM32F103C_H_) && !defined(ARDUINO_BLUEPILL_F103C8)
-
-// comment out unused bitmaps to reduce code space used
-#include "bitmaps/Bitmaps104x212.h" // 2.13" b/w flexible GDEW0213I5F
-#include "bitmaps/Bitmaps128x296.h" // 2.9"  b/w
-#include "bitmaps/Bitmaps176x264.h" // 2.7"  b/w
-#include "bitmaps/Bitmaps240x416.h" // 3.71"  b/w
-#include "bitmaps/Bitmaps400x300.h" // 4.2"  b/w
-#include "bitmaps/Bitmaps800x480.h" // 7.5"  b/w
-#include "bitmaps/Bitmaps4g104x212.h" // 2.13" b/w flexible GDEW0213I5F
-#include "bitmaps/Bitmaps4g128x296.h" // 2.9"  b/w
-#include "bitmaps/Bitmaps4g176x264.h" // 2.7"  b/w
-#include "bitmaps/Bitmaps4g_I6FD_128x296.h"
-#include "bitmaps/Bitmaps4g400x300.h" // 4.2"  b/w
-#include "bitmaps/Bitmaps4g800x480.h" // 7.5"  b/w
-#include "bitmaps/WS_Bitmaps4g.h"
-// tests
-//#include "bitmaps/Bitmaps2g104x104.h"
-
-#else
-
-// select only one to fit in code space
-//#include "bitmaps/Bitmaps104x212.h" // 2.13" b/w flexible GDEW0213I5F
-//#include "bitmaps/Bitmaps128x296.h" // 2.9"  b/w
-//#include "bitmaps/Bitmaps176x264.h" // 2.7"  b/w
-//#include "bitmaps/Bitmaps240x416.h" // 3.71"  b/w
-//#include "bitmaps/Bitmaps400x300.h" // 4.2"  b/w
-////#include "bitmaps/Bitmaps4g400x300.h" // 4.2"  b/w // too big
-
-#endif
-
-#if defined(ARDUINO_ARCH_RP2040) && defined(ARDUINO_RASPBERRY_PI_PICO)
-// SPI pins used by GoodDisplay DESPI-PICO. note: steals standard I2C pins PIN_WIRE_SDA (6), PIN_WIRE_SCL (7)
-// uncomment next line for use with GoodDisplay DESPI-PICO.
-arduino::MbedSPI SPI0(4, 7, 6); // need be valid pins for same SPI channel, else fails blinking 4 long 4 short
-#endif
-
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
   Serial.println("setup");
   delay(100);
-    pinMode(POWER, OUTPUT);    // Set epaper transistor as output
+  pinMode(POWER, OUTPUT);    // Set epaper transistor as output
   digitalWrite(POWER, HIGH); // Turn ON display
   delay(100);     
 
-#if defined(ARDUINO_ARCH_RP2040) && defined(ARDUINO_RASPBERRY_PI_PICO)
-  // uncomment next line for use with GoodDisplay DESPI-PICO, or use the extended init method
-  display.epd2.selectSPI(SPI0, SPISettings(4000000, MSBFIRST, SPI_MODE0));
-  // uncomment next 2 lines to allow recovery from configuration failures
-  pinMode(15, INPUT_PULLUP); // safety pin
-  while (!digitalRead(15)) delay(100); // check safety pin for fail recovery
-#endif
   display.init(115200);
   helloWorld();
   delay(1000);
-  //display.writeScreenBuffer(0xff); display.refresh(); display.powerOff(); return;
-  //display.epd2.drawGreyLevels(); return;
   helloFourGreyLevels();
   delay(1000);
-  //return;
   helloArduino();
   delay(1000);
   helloEpaper();
   delay(1000);
-  //helloValue(123.9, 1);
-  //delay(1000);
   showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
   delay(1000);
   drawBitmaps();
-  //display.powerOff(); return;
   showGreyLevels();
-  //display.epd2.drawGreyLevels(); delay(3000);
-  //display.powerOff(); return;
-  if (display.epd2.hasPartialUpdate)
-  {
+  if (display.epd2.hasPartialUpdate) {
     showPartialUpdate();
     delay(1000);
-  } // else // on GDEW0154Z04 only full update available, doesn't look nice
-  //drawCornerTest();
-  //showBox(16, 16, 48, 32, false);
-  //showBox(16, 56, 48, 32, true);
+  } 
   display.powerOff();
   deepSleepTest();
   Serial.println("setup done");
@@ -609,404 +552,9 @@ void showPartialUpdate()
 }
 
 
-void drawBitmaps()
-{
+void drawBitmaps() {
   display.setFullWindow();
   display.setRotation(0);
-#ifdef _GxBitmaps104x212_H_
-  drawBitmaps104x212();
-#endif
-#ifdef _GxBitmaps128x250_H_
-  drawBitmaps128x250();
-#endif
-#ifdef _GxBitmaps128x296_H_
-  drawBitmaps128x296();
-#endif
-#ifdef _GxBitmaps176x264_H_
-  drawBitmaps176x264();
-#endif
-#ifdef _GxBitmaps400x300_H_
-  drawBitmaps400x300();
-#endif
-#ifdef _GxBitmaps800x480_H_
-  drawBitmaps800x480();
-#endif
-  // 4 grey levels
-#ifdef _WS_Bitmaps4g_H_
-  drawWsBitmaps4g();
-#endif
-#ifdef _GxBitmaps4g104x212_H_
-  drawBitmaps4g104x212();
-#endif
-#ifdef _GxBitmaps4g128x296_H_
-  drawBitmaps4g128x296();
-#endif
-#ifdef _GxBitmaps4g_I6FD_128x296_H_
-  drawBitmaps4g_I6FD_128x296();
-#endif
-#ifdef _GxBitmaps4g176x264_H_
-  drawBitmaps4g176x264();
-#endif
-#ifdef _GxBitmaps4g400x300_H_
-  drawBitmaps4g400x300();
-#endif
-#ifdef _GxBitmaps4g800x480_H_
-  drawBitmaps4g800x480();
-#endif
-#ifdef _GxBitmaps2g104x104_H_
-  drawBitmaps2g104x104();
-#endif
-}
-
-#ifdef _GxBitmaps104x212_H_
-void drawBitmaps104x212()
-{
-#if !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    WS_Bitmap104x212, Bitmap104x212_1, Bitmap104x212_2, Bitmap104x212_3
-  };
-#else
-  const unsigned char* bitmaps[] =
-  {
-    WS_Bitmap104x212, Bitmap104x212_1, Bitmap104x212_2, Bitmap104x212_3
-  };
-#endif
-  if ((display.epd2.WIDTH == 104) && (display.epd2.HEIGHT == 212) && !display.epd2.hasColor)
-  {
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-  }
-  else
-  {
-    int16_t x = (int16_t(display.epd2.WIDTH) - 104) / 2;
-    int16_t y = (int16_t(display.epd2.HEIGHT) - 212) / 2;
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.drawImage(bitmaps[i], x, y, 104, 212, true, false, true);
-      delay(2000);
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps128x296_H_
-void drawBitmaps128x296()
-{
-#if !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap128x296_1, logo128x296, first128x296, second128x296, third128x296
-  };
-#else
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap128x296_1, logo128x296 //, first128x296, second128x296, third128x296
-  };
-#endif
-  if ((display.epd2.WIDTH == 128) && (display.epd2.HEIGHT == 296) && !display.epd2.hasColor)
-  {
-    bool m = display.mirror(true);
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawInvertedBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-    display.mirror(m);
-  }
-}
-#endif
-
-#ifdef _GxBitmaps176x264_H_
-void drawBitmaps176x264()
-{
-#if !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap176x264_1, Bitmap176x264_2, Bitmap176x264_3, Bitmap176x264_4, Bitmap176x264_5
-  };
-#else
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap176x264_1, Bitmap176x264_2 //, Bitmap176x264_3, Bitmap176x264_4, Bitmap176x264_5
-  };
-#endif
-  if ((display.epd2.WIDTH == 176) && (display.epd2.HEIGHT == 264) && !display.epd2.hasColor)
-  {
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawInvertedBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps240x416_H_
-void drawBitmaps240x416()
-{
-#if !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap240x416_1, Bitmap240x416_2, Bitmap240x416_3
-  };
-#else
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap240x460_1, Bitmap240x460_2, Bitmap240x460_3
-  };
-#endif
-  if ((display.epd2.WIDTH == 240) && (display.epd2.HEIGHT == 416) && !display.epd2.hasColor)
-  {
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawInvertedBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps400x300_H_
-void drawBitmaps400x300()
-{
-#if !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap400x300_1, Bitmap400x300_2
-  };
-#else
-  const unsigned char* bitmaps[] = {}; // not enough code space
-#endif
-  if ((display.epd2.WIDTH == 400) && (display.epd2.HEIGHT == 300) && !display.epd2.hasColor)
-  {
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawGreyPixmap(Bitmap400x300_1, 1, 0, 0, 400, 300);
-    }
-    while (display.nextPage());
-    delay(2000);
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawInvertedBitmap(0, 0, bitmaps[i], display.epd2.WIDTH, display.epd2.HEIGHT, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps800x480_H_
-void drawBitmaps800x480()
-{
-#if defined(ARDUINO_UNOR4_MINIMA) || defined(ARDUINO_UNOR4_WIFI)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap800x480_3, Bitmap800x480_4
-  };
-#elif !defined(__AVR)
-  const unsigned char* bitmaps[] =
-  {
-    Bitmap800x480_1, Bitmap800x480_2, Bitmap800x480_3, Bitmap800x480_4
-  };
-#else
-  const unsigned char* bitmaps[] = {}; // not enough code space
-#endif
-  if ((display.epd2.WIDTH == 800) && (display.epd2.HEIGHT == 480))
-  {
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawBitmap(0, 0, bitmaps[i], 800, 480, GxEPD_BLACK);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-    for (uint16_t i = 0; i < sizeof(bitmaps) / sizeof(char*); i++)
-    {
-      //display.drawImage(bitmaps[i], 0, 0, 800, 480, true, false, true); delay(2000);
-      //display.writeImage(bitmaps[i], 0, 0, 800, 480, true, false, true); display.refresh(false); delay(2000);
-    }
-    if (display.epd2.panel == GxEPD2_4G::GDEW075T7)
-    {
-      // avoid ghosting caused by OTP waveform
-      display.clearScreen();
-      display.refresh(false); // full update
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g104x212_H_
-void drawBitmaps4g104x212()
-{
-  if ((display.epd2.WIDTH >= 104) && (display.epd2.HEIGHT >= 212))
-  {
-    display.setFullWindow();
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawGreyPixmap(Bitmap4g104x212_1, 4, 0, 0, 104, 212);
-    }
-    while (display.nextPage());
-    delay(2000);
-    display.epd2.drawImage_4G(Bitmap4g104x212_1, 4, 0, 0, 104, 212, false, false, true);
-    delay(2000);
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g128x296_H_
-void drawBitmaps4g128x296()
-{
-  if ((display.epd2.WIDTH >= 128) && (display.epd2.HEIGHT >= 296))
-  {
-    display.setFullWindow();
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawGreyPixmap(Bitmap4g128x296_1, 4, 0, 0, 128, 296);
-    }
-    while (display.nextPage());
-    delay(2000);
-    display.epd2.drawImage_4G(Bitmap4g128x296_1, 4, 0, 0, 128, 296, false, false, true);
-    delay(2000);
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g_I6FD_128x296_H_
-void drawBitmaps4g_I6FD_128x296()
-{
-  if ((display.epd2.WIDTH >= 128) && (display.epd2.HEIGHT >= 296))
-  {
-    display.writeScreenBuffer();
-    display.epd2.drawImage_4G(Bitmap4g128x296_I6FD, 4, 0, 0, 128, 296, false, false, true); delay(2000);
-    display.drawImage(Bitmap128x296_I6FD_1, 0, 0, 128, 296, false, false, true); delay(2000);
-    display.drawImage(Bitmap128x296_I6FD_2, 0, 0, 128, 296, false, false, true); delay(2000);
-    display.drawImage(Bitmap128x296_run6, 0, 0, 128, 296, false, false, true); delay(2000);
-    display.drawImage(Bitmap128x296_run5, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run1, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run2, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run3, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run4, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run5, 0, 0, 128, 296, false, false, true); delay(500);
-    display.drawImage(Bitmap128x296_run6, 0, 0, 128, 296, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x64_man1, 24, 0, 80, 64, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x64_man2, 24, 0, 80, 64, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x64_man3, 24, 0, 80, 64, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x64_man4, 24, 0, 80, 64, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x64_man5, 24, 0, 80, 64, false, false, true); delay(2000);
-    display.drawImage(Bitmap24x80_gooddisplay, 128 - 24, (296 - 80) / 2, 24, 80, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x24_gooddisplay1, 128 - 80, 296 - 24, 80, 24, false, false, true); delay(2000);
-    display.drawImage(Bitmap80x24_gooddisplay2, 128 - 80, 296 - 24, 80, 24, false, false, true); delay(2000);
-    display.drawImage(Bitmap32x64_num1, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num2, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num3, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num4, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num5, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num6, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num7, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num8, 0, 120, 32, 64, false, false, true); delay(500);
-    display.drawImage(Bitmap32x64_num9, 0, 120, 32, 64, false, false, true); delay(500);
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g176x264_H_
-void drawBitmaps4g176x264()
-{
-  if ((display.epd2.WIDTH >= 176) && (display.epd2.HEIGHT >= 264))
-  {
-    display.setFullWindow();
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawGreyPixmap(Bitmap4g176x264_1, 4, 0, 0, 176, 264);
-    }
-    while (display.nextPage());
-    delay(2000);
-    display.epd2.drawImage_4G(Bitmap4g176x264_1, 4, 0, 0, 176, 264, false, false, true);
-    delay(2000);
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g400x300_H_
-void drawBitmaps4g400x300()
-{
-  if ((display.epd2.WIDTH >= 128) && (display.epd2.HEIGHT >= 296))
-  {
-    display.setFullWindow();
-    display.setFont(&FreeMonoBold9pt7b);
-    display.setTextColor(GxEPD_BLACK);
-    for (uint16_t r = 0; r < 4; r++)
-    {
-      display.setRotation(r);
-      display.firstPage();
-      do
-      {
-        display.fillScreen(GxEPD_WHITE);
-        display.drawGreyPixmap(Bitmap4g400x300_1, 4, 0, 0, 400, 300);
-        display.setCursor(display.width() / 2, display.height() / 2);
-        display.print(r);
-      }
-      while (display.nextPage());
-      delay(2000);
-    }
-    if ((display.epd2.WIDTH >= 400) && (display.epd2.HEIGHT >= 300))
-    {
-      display.writeScreenBuffer();
-      display.epd2.drawImage_4G(Bitmap4g400x300_1, 4, (display.epd2.WIDTH - 400) / 2, (display.epd2.HEIGHT - 300) / 2, 400, 300, false, false, true);
-      delay(2000);
-    }
-  }
-}
-#endif
-
-#ifdef _GxBitmaps4g800x480_H_
-void drawBitmaps4g800x480()
-{
   if ((display.epd2.WIDTH == 800) && (display.epd2.HEIGHT == 480))
   {
     //Serial.print("sizeof(Bitmap4g800x480_1) is "); Serial.println(sizeof(Bitmap4g800x480_1));
@@ -1014,69 +562,3 @@ void drawBitmaps4g800x480()
     delay(2000);
   }
 }
-#endif
-
-#ifdef _WS_Bitmaps4g_H_
-void drawWsBitmaps4g()
-{
-  //Serial.println("drawWsBitmaps4g()");
-  //Serial.print("sizeof(WS_Bitmap4g200x150) is "); Serial.println(sizeof(WS_Bitmap4g200x150));
-  if ((display.epd2.WIDTH >= 200) && (display.epd2.HEIGHT >= 150))
-  {
-    //    display.setRotation(0);
-    //    display.setFullWindow();
-    //    display.firstPage();
-    //    do
-    //    {
-    //      display.fillScreen(GxEPD_WHITE);
-    //      display.drawGreyPixmap(WS_Bitmap4g200x150, 2, (display.epd2.WIDTH - 200) / 2, (display.epd2.HEIGHT - 150) / 2, 200, 150);
-    //    }
-    //    while (display.nextPage());
-    //    delay(2000);
-    display.writeScreenBuffer();
-    display.epd2.drawImage_4G(WS_Bitmap4g200x150, 2, (display.epd2.WIDTH - 200) / 2, (display.epd2.HEIGHT - 150) / 2, 200, 150, false, false, true);
-    delay(2000);
-  }
-  if ((display.epd2.WIDTH >= 176) && (display.epd2.HEIGHT >= 264))
-  {
-    display.writeScreenBuffer();
-    display.epd2.drawImage_4G(WS_Bitmap4g176x264, 2, (display.epd2.WIDTH - 176) / 2, (display.epd2.HEIGHT - 264) / 2, 176, 264, false, false, true);
-    delay(2000);
-  }
-  if ((display.epd2.WIDTH >= 128) && (display.epd2.HEIGHT >= 296))
-  {
-    display.writeScreenBuffer();
-    display.epd2.drawImage_4G(WS_Bitmap4g128x296, 2, (display.epd2.WIDTH - 128) / 2, (display.epd2.HEIGHT - 296) / 2, 128, 296, false, false, true);
-    delay(2000);
-  }
-  if ((display.epd2.WIDTH >= 400) && (display.epd2.HEIGHT >= 300))
-  {
-    display.writeScreenBuffer();
-    display.epd2.drawImage_4G(WS_Bitmap4g400x300, 2, (display.epd2.WIDTH - 400) / 2, (display.epd2.HEIGHT - 300) / 2, 400, 300, false, false, true);
-    delay(2000);
-  }
-  //Serial.println("drawWsBitmaps4g() done");
-}
-#endif
-
-#ifdef _GxBitmaps2g104x104_H_
-void drawBitmaps2g104x104()
-{
-  //Serial.print("sizeof(Bitmap2g104x104) is "); Serial.println(sizeof(Bitmap2g104x104));
-  display.writeScreenBuffer();
-  display.epd2.drawImage_4G(Bitmap2g104x104, 2, display.epd2.WIDTH / 2 - 52, display.epd2.HEIGHT / 2 - 52, 104, 104, false, false, true);
-  delay(2000);
-  for (uint16_t r = 0; r <= 4; r++)
-  {
-    display.setRotation(r);
-    display.firstPage();
-    do
-    {
-      display.fillScreen(GxEPD_WHITE);
-      display.drawGreyPixmap(Bitmap2g104x104, 2, display.width() / 2 - 52, display.height() / 2 - 52, 104, 104);
-    }
-    while (display.nextPage());
-    delay(2000);
-  }
-}
-#endif
