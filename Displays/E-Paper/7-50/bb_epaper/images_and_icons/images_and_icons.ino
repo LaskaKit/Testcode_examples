c38616cda83a4fd5a661cd17e02b05d79e9cddf9
Konstantin Láska <konstantin.laska@icloud.com>
2025-05-05 14:43:34 +0200

    Update images_and_icons.ino

--- Displays/E-Paper/7-50/bb_epaper/images_and_icons/images_and_icons.ino
+++ Displays/E-Paper/7-50/bb_epaper/images_and_icons/images_and_icons.ino
@@ -18,8 +18,8 @@
 #include "smiley.h" // 100x100 compressed image
 
 BBEPAPER bbep(EP75_800x480);
-#define ESPink_V2     //for version v2.6 and earlier
-//#define ESPink_V3     //for version v3.0 and above
+//#define ESPink_V2     //for version v2.6 and earlier
+#define ESPink_V3     //for version v3.0 and above
 
 #ifdef ESPink_V2
 #define MOSI_PIN  23
