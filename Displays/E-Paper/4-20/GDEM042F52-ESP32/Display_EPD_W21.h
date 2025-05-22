fc97b01dfae8031e0c6912a4e95413f5e62a30d1
Konstantin Láska <konstantin.laska@icloud.com>
2025-05-22 12:33:19 +0200

    first commit

---
+++ Displays/E-Paper/4-20/GDEM042F52-ESP32/Display_EPD_W21.h
@@ -0,0 +1,34 @@
+#ifndef _DISPLAY_EPD_W21_H_
+#define _DISPLAY_EPD_W21_H_
+
+//2bit
+#define black   0x00  /// 00
+#define white   0x01  /// 01
+#define yellow  0x02  /// 10
+#define red     0x03  /// 11
+
+
+#define Source_BITS     400
+#define Gate_BITS   300
+#define ALLSCREEN_BYTES   Source_BITS*Gate_BITS/4
+
+
+//EPD
+void EPD_init(void);
+void PIC_display (const unsigned char* picData);
+void EPD_sleep(void);
+void EPD_update(void);
+void lcd_chkstatus(void);
+
+void Display_All_Black(void);
+void Display_All_White(void);
+void Display_All_Yellow(void);
+void Display_All_Red(void);
+
+void Acep_color(unsigned char color);
+//Fast
+void EPD_init_Fast(void);
+#endif
+/***********************************************************
+            end file
+***********************************************************/
---
+++ Displays/E-Paper/4-20/GDEM042F52-ESP32/GDEM042F52-ESP32.ino
@@ -0,0 +1,87 @@
+/* Display test for Good Display GDEM042F52
+ * example from Good Display is used
+ * 
+ * Board:   LaskaKit ESPink ESP32 e-Paper   https://www.laskakit.cz/laskakit-espink-esp32-e-paper-pcb-antenna/
+            DIP: 1 - ON, 2 - OFF
+ * Display: Good Display GDEM042F52         https://www.laskakit.cz/good-display-gdem042f52-4-2--400x300-epaper-displej-4-barvy/
+ * Email:podpora@laskakit.cz
+ * Web:laskakit.cz
+ */
+
+// ! For the board settings see Display_EPD_W21_spi.h
+
+#include <SPI.h>
+//EPD
+#include "Display_EPD_W21_spi.h"
+#include "Display_EPD_W21.h"
+#include "Ap_29demo.h"  
+
+void setup() {
+
+  // turn on power to display
+  pinMode(POWER, OUTPUT);
+  digitalWrite(POWER, HIGH);   // turn the Display Power on (HIGH is the voltage level)
+  delay(100);   
+
+  pinMode(BUSY, INPUT);
+  pinMode(RST, OUTPUT);
+  pinMode(DC, OUTPUT);
+  pinMode(CS, OUTPUT);
+  //SPI
+  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0)); 
+  SPI.begin (SCK, MISO, MOSI, CS);  
+}
+//Tips//
+/*
+1.Flickering is normal when EPD is performing a full screen update to clear ghosting from the previous image so to ensure better clarity and legibility for the new image.
+2.There will be no flicker when EPD performs a partial update.
+3.Please make sue that EPD enters sleep mode when update is completed and always leave the sleep mode command. Otherwise, this may result in a reduced lifespan of EPD.
+4.Please refrain from inserting EPD to the FPC socket or unplugging it when the MCU is being powered to prevent potential damage.)
+5.Re-initialization is required for every full screen update.
+6.When porting the program, set the BUSY pin to input mode and other pins to output mode.
+*/
+void loop() {
+#if 1//Full screen update demostration.
+
+   /************Full display*******************/
+    EPD_init(); //Full screen update initialization.
+    PIC_display(gImage_1);//To Display one image using full screen update.
+    EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
+    delay(5000); //Delay for 5s.
+    		/************Fast update mode(12s)*******************/
+	#if 1 //Fast update demostration.	
+		EPD_init_Fast(); //Fast screen update initialization.
+		PIC_display(gImage_1);//To Display one image using full screen update.
+		EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
+    delay(5000); //Delay for 5s.	
+	#endif		
+  #if 0//Demonstration of Display 4 colors, to enable this feature, please change 0 to 1.
+   /************Full display*******************/
+    EPD_init(); //Full screen update initialization.
+    Display_All_Black();//To Display one image using full screen update.
+    EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
+    delay(3000); //Delay for 3s.
+    
+    EPD_init(); //Full screen update initialization.
+    Display_All_Yellow();//To Display one image using full screen update.
+    EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
+    delay(3000); //Delay for 3s.
+
+    EPD_init(); //Full screen update initialization.
+    Display_All_Red();//To Display one image using full screen update.
+    EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
+    delay(3000); //Delay for 3s.  
+  #endif    
+    EPD_init(); //Full screen update initialization.
+    Display_All_White(); //Clear screen function.
+    EPD_sleep();//Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.. 
+        
+#endif        
+    while(1); // The program stops here         
+   
+}
+
+
+
+
+//////////////////////////////////END//////////////////////////////////////////////////
